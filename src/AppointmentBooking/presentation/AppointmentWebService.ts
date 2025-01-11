39fcb4d259ea74341384b8ac035461f867e3e5a6
Yousef Shaban <yousefshaban.eg@gmail.com>
2025-01-11 16:36:48 +0200

    Implement Prisma Schema and Repository

--- src/AppointmentBooking/presentation/AppointmentWebService.ts
+++ src/AppointmentBooking/presentation/AppointmentWebService.ts
@@ -1,10 +1,12 @@
 import { InMemoryAppointmentRepository } from "../infrastructure/InMemoryAppointmentRepository";
+import { PrismaAppointmentRepository } from "../infrastructure/PrismaAppointmentRepository";
 import { CreateAppointmentUseCase } from "../application/CreateAppointmentUseCase";
 import { CreateAppointmentController } from "./CreateAppointmentController";
+import { PrismaClient } from "@prisma/client";
 
 export class AppointmentWebService {
  static createController(): CreateAppointmentController {
-  const repo = new InMemoryAppointmentRepository();
+  const repo = new PrismaAppointmentRepository(new PrismaClient());
   const useCase = new CreateAppointmentUseCase(repo);
   return new CreateAppointmentController(useCase);
  }
--- src/AppointmentBooking/presentation/CreateAppointmentController.ts
+++ src/AppointmentBooking/presentation/CreateAppointmentController.ts
@@ -6,18 +6,22 @@ export class CreateAppointmentController {
  constructor(private readonly _usecase: CreateAppointmentUseCase) {}
 
  public async handle(req: Request, res: Response) {
-  const data = {
-   patientId: req.body.patientId,
-   patientName: req.body.patientName,
-   slotId: req.body.slotId,
-  };
+  try {
+   const data = {
+    patientId: req.body.patientId,
+    patientName: req.body.patientName,
+    slotId: req.body.slotId,
+   };
 
-  const result = await this._usecase.execute(data);
+   const result = await this._usecase.execute(data);
 
-  const response: CreateAppointmentDto = new CreateAppointmentDto(
-   result.appointmentId
-  );
+   const response: CreateAppointmentDto = new CreateAppointmentDto(
+    result.appointmentId
+   );
 
-  return res.status(201).json(response);
+   return res.status(201).json(response);
+  } catch (err: any) {
+   return res.status(500).json({ message: err.message || "Server Error" });
+  }
  }
 }
